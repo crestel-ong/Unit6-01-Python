d52ee25cff4577b5e7cdb5b521f7874f20bd2e46
crestel-ong <crestel.ong@mths.ca>
2021-10-21 16:39:43 +0000

    good

--- average.py
+++ average.py
@@ -11,14 +11,15 @@ def main():
     # this function uses an array
 
     all_numbers_list = []
+    all_numbers_added = 0
 
     # input
     for loop_counter in range(0, 10):
         single_number = random.randint(1, 100)
         all_numbers_list.append(single_number)
+        all_numbers_added = all_numbers_added + single_number
         print("The random number is: {0}".format(single_number))
 
-    all_numbers_added = sum(all_numbers_list)
     average = all_numbers_added / len(all_numbers_list)
     average = round(average, 2)
     print("")
